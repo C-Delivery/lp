55b9e703873658530c6a5185e7730fa5645b23c5
IbukiEguchi <eguchinkl@gmail.com>
2020-03-21 14:11:46 +0900

    お問い合わせページの追加

---
+++ pages/contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+       <v-btn color="purple lighten-1" large>お問い合わせ</v-btn>
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+
+
+
