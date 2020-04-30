d9a0f1cc78c2169ef649cf0ba3c8919394e4ffd7
muscle-rabbit <bitsukun75@gmail.com>
2020-05-01 01:54:08 +0900

    nuxt を githubpages に対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/lp/favicon.ico' }
     ]
   },
   /*
@@ -64,6 +64,9 @@ export default {
       }
     }
   },
+  router: {
+    base: '/lp/'
+  },
   /*
   ** Build configuration
   */
@@ -71,7 +74,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
